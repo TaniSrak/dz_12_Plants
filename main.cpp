ae44a372b85c25592d8df880d04b7b42518a488a
Татьяна Кожевникова <tatanakozevnikova786@gmail.com>
2024-02-26 18:27:46 +0500

    исправила полностью проект

--- main.cpp
+++ main.cpp
@@ -2,119 +2,98 @@
 #include <string>
 #include <vector>
 
-// Базовый класс для растений
-class Plants {
+using namespace std;
+
+class Harvest
+{
+public:
+	virtual int GetWeight()=0;
+	virtual std::string GetName()=0;
+	virtual std::string GetColor()=0;
+
 protected:
-    std::string size;
-    std::string color;
-    int maxFruits;
-
-public:
-    Plants(std::string s, std::string c, int max) : size(s), color(c), maxFruits(max) {}
-
-    virtual ~Plants() {}
-
-    // Виртуальная функция для сбора плодов
-    virtual void collectFruits() = 0;
-
-    std::string getSize() const { return size; }
-
-    std::string getColor() const { return color; }
-
-    int getMaxFruits() const { return maxFruits; }
-
-    int getAvailableFruits(int currentFruits) const {
-        return maxFruits - currentFruits;
-    }
+	std::string name_;
+	int weight_;
+	std::string color_;
 };
 
-// Базовый класс для плодов
-class Harvest {
+class Apple : public Harvest
+{
+public:
+	int GetWeight() override
+	{
+		return weight_;
+	}
+
+	std::string GetName() override
+	{
+		return name_;
+	}
+
+	std::string GetColor() override
+	{
+		return color_;
+	}
+
+	Apple(std::string name, int weight, std::string color)
+	{
+		name_ = name;
+		weight_ = weight;
+		color_ = color;
+	}
+
+};
+
+class Tree
+{
+public:
+	virtual Harvest* GetHarvest()=0;
+
+	Tree(int harvestCount, std::string color, std::string size)
+	{
+		harvestCount_ = harvestCount;
+		color_ = color;
+		size_ = size;
+	}
 protected:
-    std::string name;
-    double weight;
-    std::string color;
-
-public:
-    Harvest(std::string n, double w, std::string c) : name(n), weight(w), color(c) {}
-
-    std::string getName() const { return name; }
-
-    double getWeight() const { return weight; }
-
-    std::string getColor() const { return color; }
+	int harvestCount_;
+	std::string color_;
+	std::string size_;
 };
 
-// Класс яблоня
-class AppleTree : public Plants {
-private:
-    int currentFruits;
+class AppleTree : public Tree
+{
 public:
-    AppleTree(std::string s, std::string c, int max) : Plants(s, c, max) {}
+	Harvest* GetHarvest() 
+	{
+		if (harvestCount_ > 0)
+		{
+			harvestCount_--;
+			return new Apple("Apple", 50, "Red");
+		}
+		else
+		{
+			return nullptr;
+		}
+	}
 
-    // Метод для добавления плодов на дерево
-    void addFruits(int num) {
-        currentFruits -= num;
-    }
-    // Метод для срывания яблок с дерева
-    void pickApples(int num) {
-        if (num <= currentFruits) {
-            std::cout << "Сорвано " << num << " яблок с " << size << " яблони" << std::endl;
-            currentFruits -= num;
-        }
-        else {
-            std::cout << "С " << size << " яблони сорвали " << currentFruits << " яблок" << std::endl;
-            currentFruits = 0;
-        }
-    }
-
-    // Реализация сбора плодов для яблони
-    void collectFruits() override {
-        std::cout << "Собраны яблоки с " << size << " яблони, " << color << " цвета." << std::endl;
-        currentFruits = 0;
-    }
-    // Метод для получения доступного количества плодов
-    int getAvailableFruits() const {
-        return Plants::getAvailableFruits(currentFruits);
-    }
+	AppleTree(int harvestCount, std::string color, std::string size) 
+		: Tree(harvestCount, color, size) {}
 };
 
-// Класс плода яблони
-class Apple : public Harvest {
-public:
-    Apple(std::string n, double w, std::string c) : Harvest(n, w, c) {}
-};
+int main(){
 
-// Фабрика
-class AppleFactory {
-public:
-    static Apple* createApple(double weight, std::string color) {
-        return new Apple("Яблоко", weight, color);
-    }
-};
+	AppleTree at = AppleTree(5, "Green", "Big");
+	Harvest* h = at.GetHarvest();
+	if (h != nullptr)
+	{
+		std::cout << h->GetName();
+	}
 
-int main() {
-	setlocale(LC_ALL, "Russian");
-	int n;
-
-    AppleTree appleTree("большой", "зеленого", 0);
-
-    appleTree.collectFruits();
-
-    Apple* apple = AppleFactory::createApple(0.1, "зеленый");
-    std::cout << "Название плода: " << apple->getName() << std::endl;
-    std::cout << "Вес плода: " << apple->getWeight() << " кг" << std::endl;
-    std::cout << "Цвет плода: " << apple->getColor() << std::endl;
-
-    appleTree.addFruits(10);
-    std::cout << "Доступное количество плодов на яблоне: " << appleTree.getAvailableFruits() << std::endl;
-    appleTree.pickApples(5);
-    
-    std::cout << "Доступное количество плодов на яблоне после срывания: " << appleTree.getAvailableFruits() << std::endl;
+	//Apple* ha = (Apple)at.GetHarvest();
 
 
 
-    delete apple;
 
 	return 0;
 }
